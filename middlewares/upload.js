aa27a6d95cfff8915dc102130e83ea241803264e
Katysha94 <Vasilevakata76@gmail.com>
2024-01-26 02:01:19 +0200

    hw5

---
+++ middlewares/upload.js
@@ -0,0 +1,19 @@
+const path = require("node:path");
+const multer = require("multer");
+const crypto = require("node:crypto");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "..", "tmp"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+    const extname = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, extname);
+    const suffix = crypto.randomUUID();
+
+    cb(null, `${basename}-${suffix}${extname}`);
+  },
+});
+
+module.exports = multer({ storage });
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,10 +5,10 @@ const AuthController = require("../../controllers/auth");
 const validateBody = require("../../middlewares/validateBody");
 const { userSchema, subscriptionSchema } = require("../../validation/auth");
 const authMiddlewar = require("../../middlewares/auth");
+const uploadMiddlewar = require("../../middlewares/upload");
 
 router.post("/register", validateBody(userSchema), AuthController.register);
 router.post("/login", validateBody(userSchema), AuthController.login);
-module.exports = router;
 router.post("/logout", authMiddlewar, AuthController.logout);
 router.get("/current", authMiddlewar, AuthController.getCurrent);
 router.patch(
@@ -17,3 +17,12 @@ router.patch(
   validateBody(subscriptionSchema),
   AuthController.updateSubscription
 );
+
+router.patch(
+  "/avatar",
+  uploadMiddlewar.single("avatar"),
+  authMiddlewar,
+  AuthController.uploadAvatar
+);
+
+module.exports = router;
